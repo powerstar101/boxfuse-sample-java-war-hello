60abab41dcd45469dd58c5f844f1f49ffcc214c6
dheeraj <deerajkumar456@gmail.com>
2021-09-19 19:58:57 +0530

    updated

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 jobname=$1
 branchname=$2
 build_number=$3
-aws s3 cp s3://khalisi/artifacts/${jobname}/${branch_name}/${build_number} /tmp/
-sudo cp /tmp/hello-*.war /var/lib/tomcat/webapps
+aws s3 cp s3://khalisi/artifacts/${jobname}/${branch_name}/hello-${build_number}.war /tmp/
+sudo cp /tmp/hello-${build_number}.war /var/lib/tomcat/webapps
