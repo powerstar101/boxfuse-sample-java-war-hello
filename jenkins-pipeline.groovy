05a034c0337edf682001d389bf4419ae28e530d2
dheeraj <deerajkumar456@gmail.com>
2021-09-19 17:07:06 +0530

    updated

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -1,13 +1,12 @@
 pipeline{
     agent any
     stages{
-        
-        stage(build job){
+        stage("build job"){
             mvn clean package
             echo "building of job is done"
            
         }
-        stage(upload artifacts){
+        stage("upload artifacts"){
          aws s3 /target/hello-*.war s3://khalisi/artifacts/${jobname}/${branch_name}/${build_number}
         }
     }
