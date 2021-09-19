a192e2ae65aa16edb1c5dcb393f1200c920ed586
dheeraj <deerajkumar456@gmail.com>
2021-09-19 19:35:31 +0530

    updated

--- jenkins_deploy.groovy
+++ jenkins_deploy.groovy
@@ -4,7 +4,7 @@ pipeline{
         stage("installing tomcat"){
             steps{
                 sh ' sudo scp -i /tmp/dev_mum.pem tomcatinstall.sh ec2-user@$serverip:/tmp'
-               sh' sudo ssh -i /tmp/dev_mum.pem ec2-user@$serverip "chmod +x tomcatinstall.sh && bash tomcatinstall.sh"'
+               sh' sudo ssh -i /tmp/dev_mum.pem ec2-user@$serverip "chmod +x /tmp/tomcatinstall.sh && bash /tmp/tomcatinstall.sh"'
             }
         }
     }
