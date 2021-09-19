3a65e2f74ea5571239c5c41fac73de5d1a2b05ea
dheeraj <deerajkumar456@gmail.com>
2021-09-19 17:20:02 +0530

    updated

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -2,13 +2,17 @@ pipeline{
     agent any
     stages{
         stage("build job"){
+            steps{
            sh' mvn clean package'
             echo "building of job is done"
+            }
            
         }
         stage("upload artifacts"){
+            steps{
             sh 'sudo cp target/hello-*.war /var/lib/tomcat/webapps'
          //aws s3 cp target/hello-*.war s3://khalisi/artifacts/${jobname}/${branch_name}/${build_number}
+            }
         }
     }
 }
