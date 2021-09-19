09598f462f3cabd79d5178fbd0681fa83f67be4d
dheeraj <deerajkumar456@gmail.com>
2021-09-19 19:08:30 +0530

    updated

--- jenkins_deploy.groovy
+++ jenkins_deploy.groovy
@@ -3,7 +3,7 @@ pipeline{
     stages{
         stage("installing tomcat"){
             steps{
-               sh' ssh -o StrictHostKeyChecking no-i dev_mum.pem ec2-user@$serverip "bash tomcatinstall.sh"'
+               sh' ssh -o StrictHostKeyChecking no-i dev_mum.pem ec2-user@$serverip "chmod +x tomcatinstall.sh && bash tomcatinstall.sh"'
             }
         }
     }
