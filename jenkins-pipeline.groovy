fbb3b4de4fc5d4851ea86d6d58373b8173cfcfc3
dheeraj <deerajkumar456@gmail.com>
2021-09-19 17:33:26 +0530

    updated

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -10,8 +10,7 @@ pipeline{
         }
         stage("upload artifacts"){
             steps{
-            sh 'sudo cp target/hello-*.war /var/lib/tomcat/webapps'
-         //aws s3 cp target/hello-*.war s3://khalisi/artifacts/${jobname}/${branch_name}/${build_number}
+                aws s3 cp target/hello-*.war s3://khalisi/artifacts/${jobname}/${branch_name}/${build_number}
             }
         }
     }
