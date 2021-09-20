5ddc1e678eb11d5f10000e68bfce4aab1f590eb8
dheeraj <deerajkumar456@gmail.com>
2021-09-20 12:34:08 +0530

    added for loop

--- jenkins_deploy.groovy
+++ jenkins_deploy.groovy
@@ -3,7 +3,11 @@ pipeline{
     stages{
         stage("installing tomcat& deploy"){
             steps{
-                sh ''' sudo scp -i /tmp/dev_mum.pem tomcatinstall.sh deploy.sh ec2-user@$serverip:/tmp
+                
+                sh ''' 
+                     IFS=","
+                     for serverip in $servers
+                     sudo scp -i /tmp/dev_mum.pem tomcatinstall.sh deploy.sh ec2-user@$serverip:/tmp
                      sudo ssh -i /tmp/dev_mum.pem ec2-user@$serverip "chmod +x /tmp/*.sh && bash /tmp/tomcatinstall.sh && bash /tmp/deploy.sh ${jobname} ${branch_name} $build_numbers "
                      '''
             }
