2163a8ec1155df3781f9f56688a798396bf6371b
dheeraj <deerajkumar456@gmail.com>
2021-09-25 20:28:05 +0530

    updated

--- jenkinsfiledploy.groovy
+++ jenkinsfiledploy.groovy
@@ -4,7 +4,7 @@ pipeline{
         choice(name: 'Environments', choices:['dev','qa','perf','uat','prod'],description:'')
         string(name: 'serverip', defaultValue:"127.0.0.1",description:'servers')
         string(name: 'bucketname', defaultValue:'Khalisi',description:'bucket names')
-        string(name: 'jobname', defaultValue:'deploy',description:'buildjob name')
+        string(name: 'jobname', defaultValue:'build',description:'buildjob name')
         string(name: 'branch', defaultValue:'heeman',description:'branch name')
         string(name: 'buildnumber',defaultValue:'20',description:'build number')
     }
