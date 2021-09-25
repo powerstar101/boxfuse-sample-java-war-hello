26448dd02a70f58f167acc4210f9d65b7dcfc8bf
dheeraj <deerajkumar456@gmail.com>
2021-09-25 21:10:15 +0530

    updated

--- jenkinsfiledploy.groovy
+++ jenkinsfiledploy.groovy
@@ -3,7 +3,7 @@ pipeline{
     parameters{
         choice(name: 'Environments', choices:['dev','qa','perf','uat','prod'],description:'')
         string(name: 'serverip', defaultValue:"127.0.0.1",description:'servers')
-        string(name: 'bucketname', defaultValue:'Khalisi',description:'bucket names')
+        string(name: 'bucket_name', defaultValue:'Khalisi',description:'bucket names')
         string(name: 'jobname', defaultValue:'build',description:'buildjob name')
         string(name: 'branch', defaultValue:'heeman',description:'branch name')
         string(name: 'buildnumber',defaultValue:'20',description:'build number')
@@ -11,7 +11,7 @@ pipeline{
     stages{
         stage('deploy'){
             steps{
-                sh """ ssh -i /tmp/dev_mum.pem ec2-user@$serverip 'aws s3 cp s3://$bucketname/$jobname/$branch/$buildnumber/hello-*.war /tmp/ --recursive' """
+                sh """ ssh -i /tmp/dev_mum.pem ec2-user@$serverip 'aws s3 cp s3://$bucket_name/$jobname/$branch/$buildnumber/hello-*.war /tmp/ --recursive' """
             }
         }
         }
