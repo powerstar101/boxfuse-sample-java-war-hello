4299e5e5c1908be33dd83df26b8f4988d7b5bcbd
dheeraj <deerajkumar456@gmail.com>
2021-09-25 11:30:45 +0530

    update

--- jenkinsfilebuild.groovy
+++ jenkinsfilebuild.groovy
@@ -1,11 +1,11 @@
 pipeline{
     agent any
      stages{
-         stage("git clone"){
+         stage('git clone'){
              steps{
               git url:' https://github.com/tyrion9999/boxfuse-sample-java-war-hello.git'
              }
-             stage("Build"){
+             stage('Build'){
                  steps{
                      sh """ mvn clean package """
                  }
