e0d69da10fb3aa5761658980bca5c8684c213471
dheeraj <deerajkumar456@gmail.com>
2021-09-25 13:16:12 +0530

    updated

--- jenkinsfiledploy.groovy
+++ jenkinsfiledploy.groovy
@@ -2,11 +2,11 @@ pipeline{
     agent any
     parameters{
         choice(name: 'Environments', choices:['dev','qa','perf','uat','prod'],description:'')
-        string(name: 'serverip', Default Value:[''],description:'')
-        string(name: 'bucketname', Default Value:['Khalisi'],description:'')
-        string(name: 'jobname', Default Value:['deploy'],description:'')
-        string(name: 'branch', Default Value:['heeman'],description:'')
-        string(name: 'buildnumber',Default Value['20'],description:'')
+        string(name: 'serverip', defaultValue:"127.0.0.1",description:'servers')
+        string(name: 'bucketname', defaultValue:'Khalisi',description:'bucket names')
+        string(name: 'jobname', defaultValue:'deploy',description:'buildjob name')
+        string(name: 'branch', defaultValue:'heeman',description:'branch name')
+        string(name: 'buildnumber',defaultValue:'20',description:'build number')
     }
     stages{
         stage('deploy'){
