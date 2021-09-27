d4fd09b1b4a3adb476a91b202d9f241977c990f8
dheeraj <deerajkumar456@gmail.com>
2021-09-27 17:28:33 +0530

    changes

--- jenkins_deploy.groovy
+++ jenkins_deploy.groovy
@@ -7,8 +7,10 @@ pipeline{
                 sh ''' 
                      IFS=","
                      for serverip in $servers
+                     do
                      sudo scp -i /tmp/dev_mum.pem tomcatinstall.sh deploy.sh ec2-user@$serverip:/tmp
                      sudo ssh -i /tmp/dev_mum.pem ec2-user@$serverip "chmod +x /tmp/*.sh && bash /tmp/tomcatinstall.sh && bash /tmp/deploy.sh ${jobname} ${branch_name} $build_numbers "
+                     done
                      '''
             }
         }
